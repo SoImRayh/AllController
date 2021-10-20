0dc30131e184ceab72a4b8de16b6480ddceff4d4
rayh <raykah.silva@gmail.com>
2021-10-20 10:18:54 -0300

    add a user packege

--- src/main/java/com/example/allcontroller/HelloController.java
+++ src/main/java/com/example/allcontroller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("nao quero nada so quero o mundo");
     }
 }
---
+++ src/main/java/com/user/allController/User.java
@@ -0,0 +1,14 @@
+package com.user.allController;
+
+public class User {
+    User(){
+        String name = "";
+        String password = "";
+    }
+    private class CommonUser{
+    
+    }
+    private class Admin{
+
+    }
+}
