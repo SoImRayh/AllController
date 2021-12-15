7e7cfcf9468a81ba9f39f42c2979c4be41652876
Rayh <raykah.silva@gmail.com>
2021-12-15 11:11:43 -0300

    adicionei novas classes

---
+++ src/main/java/com/controllers/AdminWelcomeController.java
@@ -0,0 +1,11 @@
+package com.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AdminWelcomeController {
+
+    @FXML
+    private Label WelcomeText;
+
+}
--- src/main/java/com/controllers/TesteController.java
+++
@@ -1,6 +0,0 @@
-package com.controllers;
-
-
-public class TesteController {
-
-}
