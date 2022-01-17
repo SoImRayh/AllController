1b02587c68edd0d952d790e6a279534f7a2da318
Scyphor56 <italoteixeira56@gmail.com>
2022-01-17 20:34:53 -0300

    Adição de 6 telas:Atualização,Cadastro,Exclusão de Blanquetas e Empresas

---
+++ src/main/java/com/controllers/ControllerPaneBlanquetaAtualizar.java
@@ -0,0 +1,24 @@
+package com.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+public class ControllerPaneBlanquetaAtualizar {
+
+    @FXML
+    private TextField BlanquetaAtualizarFaca;
+
+    @FXML
+    private TextField BlanquetaAtualizarId;
+
+    @FXML
+    private TextField BlanquetaAtualizarLocalização;
+
+    @FXML
+    private TextField BlanquetaAtualizarMaquina;
+
+    @FXML
+    private TextField BlanquetaAtualizarObs;
+
+    @FXML
+    private Button ButtonBlanquetaAtualizar;
+}
