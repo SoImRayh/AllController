8f6fee9096206e0e47283123e1d96e35907ce4b9
Scyphor56 <italoteixeira56@gmail.com>
2022-01-23 19:22:37 -0300

    Criação do cadastro em processo

--- src/main/java/com/controllers/ControllerPaneProcesso.java
+++ src/main/java/com/controllers/ControllerPaneProcesso.java
@@ -1,5 +1,9 @@
 package com.controllers;
 
+import com.dao.BlanquetaDao;
+import com.dao.ProdutoDao;
+import com.model.Processo;
+import com.model.Produto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,6 +15,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ControllerPaneProcesso {
@@ -29,7 +35,14 @@ public class ControllerPaneProcesso {
 
     @FXML
     protected void cadastrar(ActionEvent actionEvent){
-
+        Processo processo = new Processo();
+        ProcessoDao dao = new ProcessoDao();
+        BlanquetaDao bdao = new BlanquetaDao();
+        ProdutoDao pdao = new ProdutoDao();
+        processo.setBlanqueta(bdao.acharPorPosicao(fieldBlanqueta.getText().toUpperCase(Locale.ROOT)));
+        processo.setProduto(pdao.pesquisarPorCodigo(fieldCodigo.getText().toUpperCase(Locale.ROOT)));
+        processo.setData(datapicker.getValue());
+        dao.salvar(processo);
 
     }
 
--- src/main/java/com/dao/ProcessoDao.java
+++ src/main/java/com/dao/ProcessoDao.java
@@ -1,13 +1,28 @@
 package com.dao;
 
+import com.infra.ConnectionFactory;
 import com.model.Processo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public class ProcessoDao implements IProcessoDao {
     @Override
     public Processo salvar(Processo processo) {
+        String sql = "Insert into processo (Fk_blanqueta_id, Fk_produto_id, data) values (?, ?, ?) ";
+        try(Connection connection = ConnectionFactory.getconection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,processo.getBlanqueta().getLocalizacao());
+            preparedStatement.setString(2,processo.getProduto().getCode());
+            preparedStatement.setDate(3,java.sql.Date.valueOf(processo.getData().toString()));
+        }
+        catch (SQLException sqlException){
+            throw new RuntimeException(sqlException);
+        }
         return null;
     }
 
